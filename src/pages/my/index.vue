40b931e47d227c8fe6c87a364aa6f6648546d7ef
gongshuncheng <1727503579@qq.com>
2020-04-30 16:46:49 +0800

    v1.0

---
+++ src/pages/my/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+   </div>
+</template>
+
+<script>
+import { formatTime } from '@/utils/index'
+import card from '@/components/card'
+
+export default {
+  components: {
+    card
+  },
+
+  data () {
+    return {
+    }
+  },
+
+  created () {
+ 
+  }
+}
+</script>
+
+<style>
+.log-list {
+  display: flex;
+  flex-direction: column;
+  padding: 40rpx;
+}
+
+.log-item {
+  margin: 10rpx;
+}
+</style>
